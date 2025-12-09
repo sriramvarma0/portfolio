982ff1e02bf8a2d9712aa24740ffcdc31b57d889
sriramvarma0 <bhupathirajusriramvarma@gmail.com>
2025-12-09 14:12:43 +0530

    config

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolios/',   // ✅ REQUIRED for your folder structure
+  base: '/portfolio/',   // ✅ REQUIRED for your folder structure
   plugins: [react()],
   server: {
     port: 5173
